c9f9a4505887c5ee9ed3838862c607a8e2c0f6ce
Andres Paulino <paulinoandresmiguel@gmail.com>
2024-08-24 17:56:27 -0400

    update config

--- electron/main.ts
+++ electron/main.ts
@@ -1,35 +1,25 @@
 import { app, BrowserWindow } from 'electron';
 import * as path from 'path';
-import * as url from 'url';
 import * as isDev from 'electron-is-dev';
 
 function createWindow() {
-  console.log('Creating window...');
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: false,
       contextIsolation: true,
       preload: path.join(__dirname, 'preload.js'),
     },
   });
 
-  const appPath = isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../index.html')}`;
-
-  console.log('Loading URL:', appPath);
-
-  win.loadURL(appPath);
+  win.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../index.html')}`);
 
   if (isDev) {
     win.webContents.openDevTools();
   }
 }
 
-app.whenReady().then(() => {
-  console.log('App is ready');
-  createWindow();
-});
+app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
